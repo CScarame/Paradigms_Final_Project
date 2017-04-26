598b37318491f8ea1db284b18779357a6a6e0958
Chris Scaramella <CScarame@nd.edu>
2017-04-25 22:23:44 -0400

    adding client and server programs

---
+++ server.py
@@ -0,0 +1,6 @@
+### Chris "Scary" Scaramella and Jamie Maher
+## Paradigms Final Project
+## Server Program
+
+# This program will always be on on a raspberry pi.  It will wait for computers to make connections
+# then facilitate a match between two computers.
