0ee5d685a2db694e888e51090f395f472d3895a2
Chris Scaramella <CScarame@nd.edu>
2017-05-08 20:14:27 +0000

    -

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
 
-URL = "10.25.247.41"
+URL = "ash.campus.nd.edu"
 
 class ConnectionGroup():
     def __init__(self):
